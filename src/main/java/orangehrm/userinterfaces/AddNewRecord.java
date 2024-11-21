42ea30b730a5e4de6b9a73e1b6b67eccee71967f
DIANA MARCELA RODRIGUEZ RODRIGUEZ <alejaandra1142@gmail.com>
2024-11-21 15:37:13 -0500

    Modification to locator

--- src/main/java/orangehrm/userinterfaces/AddNewRecord.java
+++ src/main/java/orangehrm/userinterfaces/AddNewRecord.java
@@ -13,15 +13,15 @@ public class AddNewRecord {
     public static final Target VACANCY_FIELD = Target.the("pasword field")
             .locatedBy("//div[@class='oxd-select-text-input']");
     public static final Target EMAIL_FIELD = Target.the("pasword field")
-            .located(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[3]/div[1]/div[1]/div[1]/div[2]/input[1]"));
+            .locatedBy("/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[3]/div[1]/div[1]/div[1]/div[2]/input[1]");
     public static final Target GETCONTACNUMBER_FIELD = Target.the("pasword field")
-            .located(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[3]/div[1]/div[2]/div[1]/div[2]/input[1]"));
+            .locatedBy("/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[3]/div[1]/div[2]/div[1]/div[2]/input[1]");
     public static final Target FILE_UPLOAD = Target.the("File input field")
-            .located(By.xpath("//input[@Type='file']"));
+            .locatedBy("//input[@Type='file']");
     public static final Target KEYWORDS_FIELD = Target.the("pasword field")
-            .located(By.xpath("//input[@placeholder='Enter comma seperated words...']"));
+            .locatedBy("//input[@placeholder='Enter comma seperated words...']");
     public static final Target NOTES_FIELD = Target.the("pasword field")
-            .located(By.xpath("//textarea[@placeholder='Type here']"));
+            .locatedBy("//textarea[@placeholder='Type here']");
     public static final Target CONSENT_FIELD = Target.the("pasword field")
             .locatedBy("//i[@class='oxd-icon bi-check oxd-checkbox-input-icon']");
     public static final Target BUTTON_SUBMIT= Target.the("pasword field")
